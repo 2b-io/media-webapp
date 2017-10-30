a26df5b1bae96f757a4328cb6aad6012c3d8ade3
LongLH <d@dapps.me>
2017-10-30 14:44:35 +0700

    support spread operation (preset stage-2)

--- app/app.js
+++ app/app.js
@@ -1,2 +1,18 @@
-console.log('hello world');
-console.log('xxx');
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    let props = this.props;
+
+    return (
+      <div {...props}>Hello world</div>
+    );
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('root'),
+  () => console.log('Render completed')
+);
