0b128b28b8e3f7463888d600130599249d4afe9c
LongLH <d@dapps.me>
2017-10-30 14:16:29 +0700

    basic webpack config

---
+++ internals/dev-server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const port = 3000;
+
+const webpackConfig = require('../webpack/webpack.dev.babel.js');
+
+const compiler = webpack(webpackConfig);
+const devMiddleware = webpackDevMiddleware(compiler, {
+  publicPath: webpackConfig.output.publicPath
+});
+
+app.use(devMiddleware);
+
+app.use('*', (req, res, next) => {
+  const filename = path.join(compiler.outputPath, 'index.html');
+
+  compiler.outputFileSystem.readFile(filename, (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    res
+      .set('content-type','text/html')
+      .send(result)
+      .end();
+  });
+});
+
+app.listen(port, () => console.log(`dev-server listens to :${port}`));
