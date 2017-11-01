5c69f54faad316a0706524b510d2bce366f524fd
LongLH <d@dapps.me>
2017-11-01 14:35:07 +0700

    remove react-router-redux

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,17 +1,5 @@
-import createHistory from 'history/createBrowserHistory';
 import React from 'react';
-import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
-import { Router, Route, Switch } from 'react-router-dom';
-
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
-
-import createStore from 'store';
-
-const browserHistory = createHistory();
-const store = createStore();
-
-const history = syncHistoryWithStore(browserHistory, store);
 
 class App extends React.Component {
   render() {
