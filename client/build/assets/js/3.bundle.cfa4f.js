16d089a71f3e706a72018cc9aacde965a26cc43b
LongLH <d@dapps.me>
2017-11-04 18:11:27 +0700

    restructor into client & server subdirectories

---
+++ client/build/assets/js/3.bundle.cfa4f.js
@@ -0,0 +1,23 @@
+webpackJsonp([3],{
+
+/***/ 18:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+
+
+let AppLayout = class AppLayout extends __WEBPACK_IMPORTED_MODULE_0_react___default.a.Component {
+  render() {
+    return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement('div', null);
+  }
+};
+
+
+/* harmony default export */ __webpack_exports__["default"] = (AppLayout);
+
+/***/ })
+
+});
