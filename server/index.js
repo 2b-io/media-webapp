b2ecb9c05952ccbd4d8e980c01999777cddf6db7
LongLH <d@dapps.me>
2017-11-05 15:55:09 +0700

    replace `require` with `import`

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 import express from 'express'
+import proxy from 'express-http-proxy'
 
 const app = express()
 const port = 3000
 
 if (process.env.NODE_ENV === 'development') {
-  let proxy = require('express-http-proxy')
-
   app.use(proxy('http://0.0.0.0:3001'))
 }
 
