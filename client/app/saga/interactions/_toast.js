7a2cb771ce3306c31d9143bed6d599768dcfb074
LongLH <d@dapps.me>
2018-01-31 13:46:23 +0700

    less font flash

--- client/app/saga/interactions/_toast.js
+++ client/app/saga/interactions/_toast.js
@@ -3,7 +3,7 @@ import { put } from 'redux-saga/effects'
 
 import { append, dismiss } from 'actions/message'
 
-const DURATION = 5e3
+const DURATION = 5e3 - 200
 // const DURATION = 0
 let counter = 0
 
@@ -18,7 +18,7 @@ export default function* toast(message, duration = DURATION) {
 
   if (!duration) return
 
-  yield delay(duration + 500)
+  yield delay(duration + 200)
 
   yield put(dismiss(key))
 }
--- client/app/styles/global.js
+++ client/app/styles/global.js
@@ -2,7 +2,7 @@ import prefix from 'helpers/vendor-prefix'
 import { COLOR, FONT_SIZE } from 'styles/constants'
 
 export const fontStyle = prefix({
-  fontFamily: 'Space Mono',
+  fontFamily: 'Space Mono, Consolas, monaco, monospace',
   color: COLOR.dark,
   fontSize: FONT_SIZE.normal,
   lineHeight: '25px',
