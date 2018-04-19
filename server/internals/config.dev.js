01b71cc6871a758d631eb3b3354aae9e2cbe2306
LongLH <d@dapps.me>
2018-04-19 19:40:35 +0700

    dockerize

--- server/internals/config.dev.js
+++ server/internals/config.dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   systemPort: 3003,
   webpackDevServer: 'http://0.0.0.0:3001',
-  mongodb: 'mongodb://localhost/myapp',
+  mongodb: 'mongodb://mongo/myapp',
   session: {
     secret: 'mysupersecret',
     ttl: '1h'
