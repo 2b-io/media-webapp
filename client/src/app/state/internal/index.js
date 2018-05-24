1b29d4d07565d295036539a46a5c8c70518a1052
LongLH <d@dapps.me>
2018-05-24 15:43:36 +0700

    remove unused files

--- client/src/app/state/internal/index.js
+++
@@ -1,2 +0,0 @@
-export { reducers as location } from './ducks/location'
-export { reducers as session } from './ducks/session'
--- client/src/app/state/saga/layout.js
+++ client/src/app/state/saga/layout.js
@@ -1,11 +1,2 @@
-import { types } from 'state/ducks/session'
-
-import { take } from 'redux-saga/effects'
-
 export default function*() {
-  while (true) {
-    const action = yield take(types.CREATE_COMPLETED)
-
-    console.log('ahihi')
-  }
 }
