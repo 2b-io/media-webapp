ee5bda0d419c1c4c8681c82cdd020a81981bf61d
cuongtv <truongcuong90@gmail.com>
2018-06-13 15:49:23 +0700

    - format style code

--- client/src/app/views/pages/project-list/still.jsx
+++ client/src/app/views/pages/project-list/still.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-  const ListProject = ()=>(
-    <div>
-      <h1>Projects</h1>
-   </div>
-  )
-  export default ListProject
+const ListProject = () => (
+  <div>
+    <h1>Projects</h1>
+  </div>)
+export default ListProject
