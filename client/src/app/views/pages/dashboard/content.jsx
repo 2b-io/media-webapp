ee84c8c1befe19f2c718bd8d3211baeffc500d49
cuongtv <truongcuong90@gmail.com>
2018-06-14 18:39:56 +0700

    - create base modal

--- client/src/app/views/pages/dashboard/content.jsx
+++ client/src/app/views/pages/dashboard/content.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import {Modal} from 'ui/elements'
 const Dashboard = () => (
   <main>
     <h1>Dashboard</h1>
+    <Modal>
+      asfkhasjkhfjk
+    </Modal>
   </main>
 )
 
