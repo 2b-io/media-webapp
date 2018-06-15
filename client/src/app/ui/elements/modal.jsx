60ce6fd6ae74b3330d4063a84c575be62932c95e
cuongtv <truongcuong90@gmail.com>
2018-06-15 14:15:05 +0700

    - Finish element Modal and add demo to project list

--- client/src/app/ui/elements/modal.jsx
+++ client/src/app/ui/elements/modal.jsx
@@ -1,39 +1,92 @@
 
-import React from 'react'
-import PropTypes from 'prop-types';
+import React  from 'react'
+import ReactDOM  from 'react-dom'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import {x} from 'react-icons-kit/feather/x'
 
-class modal extends React.Component {
+class Modal extends React.Component {
 
+  constructor(props) {
+     super(props);
+     this.containerModal = document.createElement('div');
+     document.body.appendChild(this.containerModal);
+   }
+
+  PropTypes = {
+    position: PropTypes.string,
+    dimmer: PropTypes.bool,
+    open: PropTypes.bool,
+    onClickOutside: PropTypes.func,
+    onClose: PropTypes.func,
+    height: PropTypes.oneOfType([ PropTypes.string, PropTypes.number]),
+    width: PropTypes.oneOfType([ PropTypes.string, PropTypes.number]),
+    fullWidth: PropTypes.bool
+  }
+
+  componentWillUnmount() {
+    document.body.removeChild(this.containerModal)
+  }
   render() {
-    return (
-      <Overlay>
-        <Modal>
-          {this.props.children}
-        </Modal>
-      </Overlay>
-    )
+    if (!this.containerModal) { return }
+    let {open,dimmer,position,height,width,fullWidth} = this.props
+    let styleModal = {
+      height: fullWidth? '90%' : height,
+      width: fullWidth? '90%' : width,
+      left: fullWidth? '51%' : '',
+    }
+    const modal = open ?  <Overlay
+      style = {{backgroundColor: dimmer? 'rgba(0,0,0,0.3)':''}}
+      onClick={()=>{
+        this.props.onClickOutside? this.props.onClickOutside() : ''
+      }}
+      >
+      <ContentModal
+        onClick={(e)=>(e.stopPropagation())}
+        className={position}
+        style={styleModal}
+        >
+        <IconClose
+          onClick={()=>{
+              this.props.onClose? this.props.onClose() : ''
+          }}>
+          x
+        </IconClose>
+        {this.props.children({...this.props})}
+      </ContentModal>
+    </Overlay> : ''
+
+    return  ReactDOM.createPortal(modal, this.containerModal)
   }
 }
-const Modal = styled.div `
+Modal.defaultProps = {
+  position:'center',
+  dimmer:true,
+  open:false,
+  fullWidth:false,
+  width:'30%',
+  }
+const ContentModal = styled.div `
+  {
     background-color: #fff;
     box-shadow: 3px 3px 3px rgba(0,0,0,0.2);
     padding: 40px 50px;
-    width: 60%;
     margin: 20px auto;
     border-radius: 2px;
-  .center {
+    align-self: flex-start;
+  }
+  &.center {
     position: absolute;
     top: 50%;
     left: 55%;
     transform: translate(-50%, -50%);
     transition: all 0.5s ease;
-    }
-  .top {
-    align-self: flex-start;
-    transition: all 0.5s ease;
+    align-self: center;
   }
-
+  &.top {
+    top: 0;
+    left: 0;
+  }
 `
 const Overlay = styled.div `
   transition: all 0.3s ease;
@@ -45,8 +98,16 @@ const Overlay = styled.div `
   top: 0;
   left: 0;
   transform: scale(1);
-  background-color: rgba(0,0,0,0.7);
+  z-index: 1;
 `
-
-
-export default modal
+const IconClose = styled.div `
+  cursor: pointer;
+  position: absolute;
+  right: 0px;
+  top: 5px;
+  font-size: 20px;
+  width: 30px;
+  height: 30px;
+  color: #282c34;
+`
+export default Modal
--- client/src/app/views/pages/project-list/content.jsx
+++ client/src/app/views/pages/project-list/content.jsx
@@ -3,13 +3,16 @@ import {mapDispatch} from 'services/redux-helpers'
 import {connect} from 'react-redux'
 import {actions} from 'state/interface'
 import {GridView} from 'ui/compounds'
+import {Modal} from 'ui/elements'
 
 class ProjectList extends React.Component {
   componentDidMount() {
       // to do
     this.props.getListProject()
   }
-
+  state = {
+    open:false
+  }
   displayProjects() {
     let {projects} = this.props
     if (projects && projects.length) {
@@ -28,6 +31,27 @@ class ProjectList extends React.Component {
     return (
       <main>
         {this.displayProjects()}
+        <Modal
+          open={this.state.open}
+          onClickOutside={(e)=>{
+           this.setState({open:false})
+        }}
+          onClose={(e)=>{
+           this.setState({open:false})
+        }}
+        >
+          {({open}) => {
+            const content =  open ? <div>
+            <p>
+              Test new modal
+            </p>
+            <button> New modal </button>
+            {this.displayProjects()}
+          </div> : ''
+          return content
+        }}
+        </Modal>
+        <button onClick={()=> { this.setState({open:true})} }>Open Modal</button>
       </main>
     )
   }
