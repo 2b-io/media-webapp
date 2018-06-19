b8708e9f9ee3fe3c2d15f0b0f6299d1c79ac8404
cuongtv <truongcuong90@gmail.com>
2018-06-19 08:35:15 +0700

    - Create base form-line

---
+++ client/src/app/ui/compounds/form/form-line/description-form.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const DescriptionForm = styled.p`
+    text-align: justify;
+    font-size: : 14px;
+    padding-bottom: 15px;
+`
+export default DescriptionForm
