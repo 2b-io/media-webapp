371ac3661c62a43cff2080349a76968d1fccfbc8
cuongtv <truongcuong90@gmail.com>
2018-06-19 15:54:16 +0700

    - Fix form-align

--- client/src/app/ui/compounds/form/form-align.jsx
+++ client/src/app/ui/compounds/form/form-align.jsx
@@ -3,11 +3,9 @@ import styled from 'styled-components'
 
 const Align = styled.div`
   text-align: ${
-    ({ center,justify,right }) => {
-      return center? 'center' : 'unset'
-      return justify? 'justify' : 'unset'
-      return right? 'right' : 'unset'
-    }
+    ({ center,justify,right }) => (
+      center? 'center' : justify? 'justify' : right? 'right' :'unset'
+    )
   };
 `
 export default  Align
