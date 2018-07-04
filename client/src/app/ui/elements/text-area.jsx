4039ab70659506b798fceccd7d5b2c21b5c46026
cuongtv <truongcuong90@gmail.com>
2018-07-04 19:00:02 +0700

    - create screen project

---
+++ client/src/app/ui/elements/text-area.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const TextArea = styled.textarea.attrs({
+  type: ({ type = 'text' }) => type
+})`
+  appearance: none;
+  background-color: transparent;
+  border: none;
+  border-bottom: 2px solid lightgray;
+  border-radius: 0;
+  outline: none;
+  padding: 10px;
+  width: 100%;
+  transition: border-bottom .6s linear;
+  &:hover, &:focus {
+    border-bottom: 2px solid black;
+  }
+`
+
+export default TextArea
--- client/src/app/views/pages/project-detail/content.jsx
+++ client/src/app/views/pages/project-detail/content.jsx
@@ -2,11 +2,78 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { selectors } from 'state/interface'
+import { Form } from 'ui/compounds'
+import { Button, TextBox, TextArea } from 'ui/elements'
 import { withParams } from 'views/router'
 
-const ProjectDetail = ({ project }) => (
+const ProjectDetail = ({ project, handleSubmit }) => (
+
   <main>
-    <h1>{ project && project.name }</h1>
+    { project && project.name &&
+      <Form handleSubmit={ handleSubmit }>
+        <label>Name</label>
+        <Form.Line>
+          <TextBox
+            type="text"
+            name="name"
+            defaultValue={ project.name }
+            placeholder="Name"
+          />
+        </Form.Line>
+        <Form.Line>
+          <label>Slug</label>
+          <TextBox
+            type="text"
+            name="slug"
+            defaultValue={ project.slug }
+            placeholder="Slug"
+          />
+        </Form.Line>
+        <Form.Line>
+          <span>Pretty origin</span>
+          <TextArea
+            rows="3"
+            type="text"
+            name="prettyOrigin"
+            placeholder="Pretty origin"
+          />
+        </Form.Line>
+        <Form.Line>
+          <span>Origins</span>
+          <TextBox
+            type="text"
+            name="origins"
+            placeholder="Origins"
+          />
+        </Form.Line>
+        <Form.Line>
+          <Form.Align center>
+            <Button>Save</Button>
+          </Form.Align>
+        </Form.Line>
+        <Form.Line>
+          <span>Presets</span>
+          <TextBox
+            type="text"
+            name="presets"
+            placeholder="Presets"
+          />
+        </Form.Line>
+        <Form.Line>
+          <span>Collaborators</span>
+          <TextBox
+            type="text"
+            name="collaborators"
+            placeholder="collaborators"
+          />
+        </Form.Line>
+        <Form.Line>
+          <Form.Align center>
+            <Button>Disabled</Button>
+          </Form.Align>
+        </Form.Line>
+      </Form>
+    }
   </main>
 )
 
