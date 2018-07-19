e83c3a959397795adaaeb33616a3dc19c4ebdd93
LongLH <d@dapps.me>
2018-07-19 14:51:15 +0700

    slide content when maximize sidebar in small screen

--- client/src/app/ui/compounds/title-bar/title.jsx
+++ client/src/app/ui/compounds/title-bar/title.jsx
@@ -2,9 +2,16 @@ import styled from 'styled-components'
 
 const Title = styled.div`
   flex-grow: 1;
-  flex-shrink: 0;
+  flex-shrink: 1;
   text-transform: uppercase;
   line-height: 2.2em;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  & > * {
+    display: inline-block;
+  }
 `
 
 export default Title
