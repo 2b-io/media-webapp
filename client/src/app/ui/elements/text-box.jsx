e1e60b1723153c55c7c8b027dd4acd3efddf8fc2
cuongtv <truongcuong90@gmail.com>
2018-07-20 14:20:07 +0700

    - Update textbox

--- client/src/app/ui/elements/text-box.jsx
+++ client/src/app/ui/elements/text-box.jsx
@@ -4,13 +4,33 @@ import styled from 'styled-components'
 import { SuccessIcon, ErrorIcon } from 'ui/icons'
 
 const Container = styled.div`
-  display: flex;
+  position: relative;
+  border-bottom: 2px solid ${
+    ({ disabled, readOnly, theme, error, valid }) => (disabled || readOnly) ?
+      theme.secondary.base : (
+        error ? theme.error.base : (
+          valid ? theme.success.base : theme.primary.base
+        )
+      )
+  };
+  &:hover, &:focus {
+    border-bottom: 2px solid ${
+      ({ disabled, readOnly, theme, error, valid }) => (disabled || readOnly) ?
+        theme.secondary.light.base : (
+          error ? theme.error.light.base : (
+            valid ? theme.success.light.base : theme.primary.light.base
+          )
+        )
+    };
+  }
 `
 const Icon = styled.div`
-  float: right;
-  padding-top: 5px;
+  position: absolute;
+  transform: translate3d(0, -50%, 0);
+  top: 50%;
+  right: ${ ({ theme }) => theme.spacing.small };
   color: ${
-    ({ theme, error }) => error ? theme.error.base :theme.success.base
+    ({ theme, error }) => error ? theme.error.base : theme.success.base
   }
 `
 const Input = styled.input.attrs({
@@ -20,30 +40,19 @@ const Input = styled.input.attrs({
   background-color: inherit;
   color: inherit;
   border: none;
-  border-bottom: 2px solid ${
-    ({ disabled, readOnly, theme, error, valid, touched }) => (disabled || readOnly) ?
-      theme.secondary.base :
-      error && touched ? theme.error.base : valid ? theme.success.base : theme.primary.base
-  };
   border-radius: 0;
   outline: none;
   padding: ${ ({ theme }) => theme.spacing.small };
+  padding-right: ${ ({ valid, error }) => valid || error && '44px' };
   width: 100%;
   transition: border-bottom .3s linear;
-
-  &:hover, &:focus {
-    border-bottom: 2px solid ${
-      ({ disabled, readOnly, theme, error, valid, touched }) => (disabled || readOnly) ?
-        theme.secondary.base :
-        error && touched ? theme.error.base : valid ? theme.success.base : theme.primary.base
-    };
-  }
 `
 const TextBox = (props) => (
-  <Container>
+  <Container { ...props } >
     <Input { ...props } />
     <Icon error={ props.error }>
-      {props.error && props.touched? <ErrorIcon  size='medium' /> : props.valid? <SuccessIcon size='medium' /> :''}
+      { props.error && <ErrorIcon size="medium" /> }
+      { props.valid && <SuccessIcon size="medium" /> }
     </Icon>
   </Container>
 )
