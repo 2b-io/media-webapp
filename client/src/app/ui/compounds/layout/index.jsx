e2f20b642e94ac9049468be360d21cabdadbc30a
LongLH <d@dapps.me>
2018-07-23 10:07:26 +0700

    optimize compounds/layout

--- client/src/app/ui/compounds/layout/index.jsx
+++ client/src/app/ui/compounds/layout/index.jsx
@@ -49,11 +49,9 @@ class Layout extends Component {
   }
 
   calculateUniqueTops() {
-    const baseTop = this.wrapper.getBoundingClientRect().top
-
     return this.layoutItems
       .map(
-        item => item.getBoundingClientRect().top - baseTop
+        item => item.getBoundingClientRect().top
       )
       .filter(
         (value, index, self) => self.indexOf(value) === index
@@ -61,10 +59,6 @@ class Layout extends Component {
   }
 
   renderItems() {
-    if (!this.wrapper) {
-      return null
-    }
-
     const tops = this.calculateUniqueTops()
 
     return (
@@ -92,7 +86,7 @@ class Layout extends Component {
     }
 
     return (
-      <Wrapper innerRef={ e => this.wrapper = e }>
+      <Wrapper>
         <Dimension>
           { this.renderItems }
         </Dimension>
