e60336867801c453b637ce5e40b20f856d002a09
cuongtv <truongcuong90@gmail.com>
2018-07-23 09:31:30 +0700

    - Format Style code

--- client/src/app/ui/elements/text-box.jsx
+++ client/src/app/ui/elements/text-box.jsx
@@ -61,7 +61,16 @@ const TextArea = styled.textarea.attrs({
 
 const TextBox = ({ valid, invalid, multiline, ...props }) => (
   <Container valid={ valid } invalid={ invalid } >
-    { multiline ? <TextArea valid={ valid } invalid={ invalid } rows={ multiline } { ...props } /> : <Input valid={ valid } invalid={ invalid } { ...props } /> }
+    { multiline ? <TextArea
+      valid={ valid }
+      invalid={ invalid }
+      rows={ multiline }
+      { ...props } /> 
+      : <Input
+        valid={ valid }
+        invalid={ invalid }
+        { ...props } />
+    }
     <Icon invalid={ invalid }>
       { invalid && <ErrorIcon size="medium" /> }
       { valid && <SuccessIcon size="medium" /> }
