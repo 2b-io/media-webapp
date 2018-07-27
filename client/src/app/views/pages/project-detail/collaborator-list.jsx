098214a357de54b5b60383d4c912347ca8e89677
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-07-27 16:16:34 +0700

    Collaborator: added button make owner

--- client/src/app/views/pages/project-detail/collaborator-list.jsx
+++ client/src/app/views/pages/project-detail/collaborator-list.jsx
@@ -4,36 +4,62 @@ import styled from 'styled-components'
 import { List } from 'ui/compounds'
 import { Button, Link } from 'ui/elements'
 
-const Item = styled.div`
+const Wrapper = styled.div`
   display: flex;
-  justify-content: space-between;
-  flex-wrap: nowrap;
 `
 
-const CollaboratorList = ({ collaborators, toProfile, session }) => (
-  <List>
-    {
-      Object.values(collaborators).map(
-        ({ _id, account, privilege }) => (
-          <List.Item key={ _id }>
-            <Item>
-              <Link href="#"
-                onClick={ () => toProfile(account._id) }
-              >
-                <span>{ account.email }</span>
-              </Link>
-              {
-                ( account._id===session.account._id ) ? <span>{ privilege }</span> : (
-                  ( privilege!== 'owner' ) && <Button plain type="submit">Make owner</Button>
-                )
-                //Dang sai logic
-              }
-            </Item>
-          </List.Item>
+const Left = styled.div`
+  flex-grow: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+const Right = styled.div`
+  flex-grow: 0;
+`
+
+const PrivilegeValue = styled.span`
+  padding: ${
+    ({ theme }) => `0 ${ theme.spacing.small }`
+  };
+`
+
+const CollaboratorList = ({ collaborators, toProfile, session }) => {
+  const signedInCollaborator = Object.values(collaborators).find(
+    ({ account }) => account._id === session.account._id
+  )
+
+  return (
+    <List>
+      {
+        Object.values(collaborators).map(
+          ({ _id, account, privilege }) => {
+
+            return (
+              <List.Item key={ _id }>
+                <Wrapper>
+                  <Left>
+                    <Link href="#" onClick={ () => toProfile(account._id) } >
+                      <span>{ account.email }</span>
+                    </Link>
+                  </Left>
+                  <Right>
+                    { privilege === 'owner' &&
+                      <PrivilegeValue>{privilege}</PrivilegeValue>
+                    }
+                    { signedInCollaborator.privilege === 'owner' && privilege === 'admin' &&
+                      <Button variant="primary">Make owner</Button>
+                    }
+                  </Right>
+                </Wrapper>
+              </List.Item>
+            )
+          }
         )
-      )
-    }
-  </List>
-)
+      }
+    </List>
+  )
+}
 
 export default CollaboratorList
