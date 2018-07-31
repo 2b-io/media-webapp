1485a0f03d31272d7d9e049bf53f39a580a43fb0
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-07-31 10:24:58 +0700

    added checkbox style

--- client/src/app/ui/elements/check-box.jsx
+++ client/src/app/ui/elements/check-box.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
 
+const border = color => css`
+  border: 1px solid ${ color.base };
+
+  &:hover, focus {
+    border: 1px solid ${ color.light.base };
+  }
+`
+const Container = styled.div`
+  position: relative;
+  transition: border .3s linear;
+  background: ${ ({ theme }) => theme.white.base };
+  color: ${ ({ theme }) => theme.white.on.base };
+  ${
+    ({ disabled, readOnly, theme }) =>
+      (disabled || readOnly) ? border(theme.secondary) : border(theme.primary)
+  };
+`
+
+const Label = styled.label`
+  font-size: 0.9em;
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 6px;
+  background: white;
+  line-height: ${ ({ theme }) => theme.spacing.medium };
+  height: ${ ({ theme }) => theme.spacing.medium };
+  padding: ${ ({ theme }) => `0 ${ theme.spacing.tiny }` };
+`
 
 const Wrapper = styled.div`
+  position: relative;
   padding-top: ${
     ({ theme }) => theme.spacing.small
   };
@@ -11,25 +41,25 @@ const Wrapper = styled.div`
   };
 `
 
-const Container = styled.div`
-  display: flex;
-`
-
 const Input = styled.input.attrs({
   type: 'checkbox'
 })`
   width: 20px;
   height: 20px;
+  display: block;
+  margin: ${ ({ theme }) => theme.spacing.small };
 `
 
 const CheckBox = ({ label, ...props }) => (
   <Wrapper>
     <Container { ...props }>
-      <label > { label } </label>
       <div>
         <Input { ...props } />
       </div>
     </Container>
+    { label &&
+      <Label>{ label }</Label>
+    }
   </Wrapper>
 )
 
