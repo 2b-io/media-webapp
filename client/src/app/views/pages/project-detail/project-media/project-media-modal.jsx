022b335a0770a3616dc404277998cf14a64ce198
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-09-04 15:33:28 +0700

    edit style

--- client/src/app/views/pages/project-detail/project-media/project-media-modal.jsx
+++ client/src/app/views/pages/project-detail/project-media/project-media-modal.jsx
@@ -8,20 +8,13 @@ import { modal } from 'views/common/decorators'
 import { mapDispatch } from 'services/redux-helpers'
 import { Panel } from 'ui/compounds'
 
-const Wrapper = styled.div`
+const WrapperImage = styled.div`
   display: flex;
   justify-content: center;
   padding: ${
     ({ theme }) => theme.spacing.medium
   }
 `
-
-const WrapperInfo = styled.div`
-  padding: ${
-    ({ theme }) => `${ theme.spacing.small } 0`
-  }
-`
-
 const Image = styled.img`
   max-height: 100%;
   @media (min-width: 300px) {
@@ -36,6 +29,27 @@ const Image = styled.img`
   max-width: 100%;
   display: block;
 `
+
+const Wrapper = styled.div`
+  padding: ${
+    ({ theme }) => `${ theme.spacing.small } 0`
+  }
+  display: flex;
+`
+
+const WrapperTitle = styled.div`
+  flex-basis: 30%;
+  flex-grow: 0;
+  flex-wrap: nowrap;
+`
+
+const WrapperInfo = styled.div`
+  flex-basis: 70%;
+  flex-grow: 0;
+  flex-wrap: nowrap;
+`
+
+
 
 const getContentLength = (contentLength) => {
   const BYTES_TO_MB = 1/1048576
@@ -52,37 +66,47 @@ const ProjectMediaModal = ({
     <Fragment>
       <Panel>
         <Panel.Content>
-          <Wrapper>
+          <WrapperImage>
             <Image
               src={ mediaInfo.path }
             />
-          </Wrapper>
+          </WrapperImage>
         </Panel.Content>
         <Panel.Footer>
-          <WrapperInfo>
-            <Nowrap>
-              Project: { mediaInfo.project }
-            </Nowrap>
-            <Nowrap>
-              Image name: { mediaInfo.id }
-            </Nowrap>
-            <Nowrap>
-              Content Type: { mediaInfo.contentType }
-            </Nowrap>
-            <Nowrap>
-              Content Length: { getContentLength(mediaInfo.contentLength) }
-            </Nowrap>
-            <Nowrap>
-              OriginUrl: <Link target="_blank" href={ mediaInfo.originUrl }>{ mediaInfo.originUrl }</Link>
-            </Nowrap>
-            <Nowrap>
-              Path: <Link target="_blank" href={ mediaInfo.path }>{ mediaInfo.path }</Link>
-            </Nowrap>
-            <Button
+          <Wrapper>
+            {
+              // <WrapperTitle>
+              //   <Nowrap>
+              //     Project: { mediaInfo.project }
+              //   </Nowrap>
+              //   <Nowrap>
+              //     Image name: { mediaInfo.id }
+              //   </Nowrap>
+              //   <Nowrap>
+              //     Content Type: { mediaInfo.contentType }
+              //   </Nowrap>
+              //   <Nowrap>
+              //     Content Length: { getContentLength(mediaInfo.contentLength) }
+              //   </Nowrap>
+              //   <Nowrap>
+              //     OriginUrl: <Link target="_blank" href={ mediaInfo.originUrl }>{ mediaInfo.originUrl }</Link>
+              //   </Nowrap>
+              //   <Nowrap>
+              //     Path: <Link target="_blank" href={ mediaInfo.path }>{ mediaInfo.path }</Link>
+              //   </Nowrap>
+              // </WrapperTitle>
+            }
+            <WrapperTitle>
+              asdas
+            </WrapperTitle>
+            <WrapperInfo>
+              dasdas
+            </WrapperInfo>
+          </Wrapper>
+          <Button
               plain
               onClick={ () => removeProjectMedia(mediaInfo.project, mediaInfo.id) }
             >Remove media</Button>
-          </WrapperInfo>
         </Panel.Footer>
       </Panel>
     </Fragment>
