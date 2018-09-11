eadc3708490e771127f3f6d701ab7641a594fd48
LongLH <d@dapps.me>
2018-09-11 11:37:02 +0700

    update project list

--- client/src/app/views/pages/project-list/content.jsx
+++ client/src/app/views/pages/project-list/content.jsx
@@ -1,74 +1,94 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 
 import { mapDispatch, mapState } from 'services/redux-helpers'
 import { actions, selectors } from 'state/interface'
-import { Panel, TitleBar } from 'ui/compounds'
-import { Button, Container, Link, MasonryLayout, Nowrap, Paragraph } from 'ui/elements'
-import { FavoriteIcon } from 'ui/icons'
+import { Card } from 'ui/elements'
+import { AddIcon } from 'ui/icons'
+import { Text } from 'ui/typo'
 
-const Project = ({ project, toProjectDetail }) => (
-  <Panel>
-    <Panel.Header>
-      <TitleBar>
-        <TitleBar.Title>
-          <Link href="/" onClick={ toProjectDetail }>
-            { project.name }
-          </Link>
-        </TitleBar.Title>
-        <TitleBar.Menu>
-          <Button plain>
-            <FavoriteIcon size="medium" />
-          </Button>
-        </TitleBar.Menu>
-      </TitleBar>
-    </Panel.Header>
-    <Panel.Content>
-      <Container>
-        <Nowrap>Slug: { project.slug }</Nowrap>
-        <Nowrap>Presets: { Object.keys(project.presets).length }</Nowrap>
-        <Nowrap>Collaborators: { Object.keys(project.collaborators).length }</Nowrap>
-      </Container>
-    </Panel.Content>
-  </Panel>
-)
+const Fab = styled.button`
+  position: fixed;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  overflow: hidden;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 40px;
+  appearance: none;
+  background: #07f;
+  color: white;
+  outline: none;
+  border: none;
+  bottom: 8px;
+  right: 8px;
+`
+
+const Layout = styled.section`
+  padding: 16px;
+  height: 100%;
+  min-height: 100%;
+  max-height: 100%;
+  overflow-y: auto;
+  background: #e6e6e6;
+`
+
+const Container = styled.div`
+  display: grid;
+  & > * {
+    min-height: 0;
+    min-width: 0;
+  }
+
+  grid-gap: 16px;
+  grid-template-columns: 100%;
+`
 
 const ProjectList = ({
   projects,
+  toCreateProject,
   toProjectDetail
 }) => {
   if (!projects || !projects.length) {
     return (
-      <main>
+      <Layout>
         <Container>
-          <Paragraph>
-            You do not have any projects yet.
-          </Paragraph>
+          <Text>You do not have any projects yet.</Text>
         </Container>
-      </main>
+      </Layout>
     )
   }
 
-  const items = projects.map(
-    project => ({
-      grid: { w: 1, h: 1 },
-      component: () => (
-        <Project key={ project._id }
-          project={ project }
-          toProjectDetail={ toProjectDetail.bind(null, project.slug) }
-        />
-      )
-    })
+  const cards = projects.map(
+    project => (
+      <Card
+        key={ project.slug }
+        onClick={ toProjectDetail.bind(null, project.slug) }
+        content={ () => (
+          <Text mostLeft mostRight>
+            { project.name }<br />
+            d1wbceeoo5msfq.cloudfront.net<br />
+            DEPLOYED
+          </Text>
+        ) }
+      />
+    )
   )
 
   return (
-    <main>
+    <Layout>
       <Container>
-        <MasonryLayout
-          items={ items }
-        />
+        { cards }
       </Container>
-    </main>
+      <Fab onClick={ toCreateProject }>
+        <AddIcon />
+      </Fab>
+    </Layout>
   )
 }
 
@@ -77,6 +97,7 @@ export default connect(
     projects: selectors.allProjects,
   }),
   mapDispatch({
+    toCreateProject: () => actions.requestLocation('/projects/create'),
     toProjectDetail: slug => actions.requestLocation(`/projects/${ slug }`)
   })
 )(ProjectList)
