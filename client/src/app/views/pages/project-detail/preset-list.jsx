82adac273e939d856f5af6da8c2b0b75a33dcd6f
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-09-24 16:01:33 +0700

    add create preset action

--- client/src/app/views/pages/project-detail/preset-list.jsx
+++ client/src/app/views/pages/project-detail/preset-list.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { Heading, TextLine } from 'ui/typo'
-import { Button, Card, ErrorBox, List, MenuMore, Paragraph } from 'ui/elements'
-import { AddIcon, EditIcon, OwnerAddIcon, ReloadIcon } from 'ui/icons'
+import { Card, List, MenuMore } from 'ui/elements'
+import { AddIcon } from 'ui/icons'
 
 const LineWithButton = styled.div`
   display: grid;
@@ -15,45 +15,48 @@ const LineWithButton = styled.div`
 `
 
 
-const PresetList = ({ presets }) => {
+const PresetList = ({
+  presets,
+  toCreatePreset
+}) => {
 
   const lists = presets ? Object.values(presets).map(
-      (preset, index) => ({
-        key: index,
-        content: () => (
-          <LineWithButton>
-            <TextLine mostLeft mostRight>
-              { preset.contentType }
-            </TextLine>
-            <MenuMore
-              name={ preset.contentType }
-              content={ () => (
-                <List
-                  items={ [
-                    {
-                      content: () => <TextLine mostLeft mostRight>Edit</TextLine>
-                    },
-                    {
-                      content: () => <TextLine mostLeft mostRight>Disable</TextLine>
-                    },
-                    {
-                      content: () => <TextLine mostLeft mostRight>Remove</TextLine>
-                    }
-                  ] }
-                />
-              ) }
-            />
-          </LineWithButton>
-        )
-      })
-    ): []
+    (preset, index) => ({
+      key: index,
+      content: () => (
+        <LineWithButton>
+          <TextLine mostLeft mostRight>
+            { preset.contentType }
+          </TextLine>
+          <MenuMore
+            name={ preset.contentType }
+            content={ () => (
+              <List
+                items={ [
+                  {
+                    content: () => <TextLine mostLeft mostRight>Edit</TextLine>
+                  },
+                  {
+                    content: () => <TextLine mostLeft mostRight>Disable</TextLine>
+                  },
+                  {
+                    content: () => <TextLine mostLeft mostRight>Remove</TextLine>
+                  }
+                ] }
+              />
+            ) }
+          />
+        </LineWithButton>
+      )
+    })
+  ): []
 
   return (
     <Card
       title={ () => <Heading mostLeft mostRight>Presets</Heading> }
-      fab={ () => <AddIcon onClick={ () => toEditProject(project.identifier) } /> }
+      fab={ () => <AddIcon onClick={ toCreatePreset } /> }
       content={ () => (
-        !!lists.length ?
+        lists.length>0 ?
           <List items={ lists } />:
           <TextLine mostLeft mostRight>No preset found</TextLine>
       ) }
