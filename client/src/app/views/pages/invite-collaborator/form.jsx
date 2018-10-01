ff96f61976968aaa75531b808ddc9efd4bb82e00
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-10-01 18:10:57 +0700

    check valid multi email

--- client/src/app/views/pages/invite-collaborator/form.jsx
+++ client/src/app/views/pages/invite-collaborator/form.jsx
@@ -4,15 +4,15 @@ import { Form } from 'ui/compounds'
 import { Button, Break } from 'ui/elements'
 import { DescriptionText } from 'ui/typo'
 import { TextArea } from 'views/common/form'
-import { validateRequired, validateEmail } from 'views/common/validate'
+import { validateRequired, validateMultiEmail } from 'views/common/validate'
 
 const InviteCollaboratorForm = ({ handleSubmit }) => (
   <Form handleSubmit={ handleSubmit }>
     <TextArea
-      label="Emails"
-      placeholder="Emails"
-      name="emails"
-      validate={ [ validateRequired, validateEmail ] }
+      label="Email list"
+      placeholder="Email to invite."
+      name="emailList"
+      validate={ [ validateRequired, validateMultiEmail ] }
     />
     <DescriptionText mostLeft mostRight>
       We&#39;ll send the persons you invited an email with the invitation detail. If they don&#39;t have an account yet, we&#39;ll create one and send instructions for setting their password.
