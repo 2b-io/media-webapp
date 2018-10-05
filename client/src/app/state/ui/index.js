2fd639ce0955309bc8cbf1ad4cad4900b0e273ff
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-10-05 09:11:15 +0700

    remove state ui

--- client/src/app/state/ui/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ChangePassword } from './change-password'
-export { default as EditProject } from './edit-project'
-export { default as GetResetCode } from './get-reset-code'
-export { default as PresetModal } from './preset-modal'
