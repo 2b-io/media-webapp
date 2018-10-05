11f786cee06c5fd8df58e51a76c954c167dec58c
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-10-05 09:19:24 +0700

    remove compounds dialog

--- client/src/app/views/common/decorators/index.jsx
+++ client/src/app/views/common/decorators/index.jsx
@@ -1,4 +1,3 @@
-export { default as dialog } from './dialog'
 export { default as modal } from './modal'
 export { default as reduxField } from './redux-field'
 export { default as stateful } from './stateful-component'
