80bf81d6c40b784107e10a72f4c26271dd1ccaba
LongLH <d@dapps.me>
2018-10-08 15:06:36 +0700

    review saga/pull-setting.js

--- client/src/app/state/saga/pull-setting.js
+++ client/src/app/state/saga/pull-setting.js
@@ -6,51 +6,65 @@ import { actions, types, selectors } from 'state/interface'
 
 const getLoop = function*() {
   while (true) {
-    const action = yield take(types['PULLSETTING/GET'])
-
     try {
+      const {
+        payload: {
+          identifier
+        }
+      } = yield take(types.pullSetting.GET)
+
       const session = yield select(selectors.currentSession)
 
       if (!session) {
-        continue
+        throw 'Unauthorized'
       }
 
-      const { identifier } = action.payload
       const pullSetting = yield PullSetting.getPullSetting(session.token, identifier)
-      yield put(actions.getPullSettingCompleted({ identifier, pullSetting }))
+
+      yield put(
+        actions.getPullSettingCompleted({
+          identifier, pullSetting
+        })
+      )
     } catch (e) {
-      yield put(actions.getPullSettingFailed(serializeError(e)))
-      continue
+      yield put(
+        actions.getPullSettingFailed(serializeError(e))
+      )
     }
   }
 }
 
 const updateLoop = function*() {
   while (true) {
-    const action = yield take(types['PULLSETTING/UPDATE'])
-
     try {
+      const {
+        payload: {
+          identifier,
+          pullSetting
+        }
+      } = yield take(types.pullSetting.UPDATE)
+
       const session = yield select(selectors.currentSession)
 
       if (!session) {
-        continue
+        throw 'Unauthorized'
       }
-
-      const { identifier, pullSetting } = action.payload
 
       const updatedPullSetting = yield PullSetting.updatePullSetting(session.token, identifier, pullSetting)
 
-      yield put(actions.updatePullSettingCompleted({
-        identifier,
-        pullSetting: updatedPullSetting
-      }))
+      yield put(
+        actions.updatePullSettingCompleted({
+          identifier,
+          pullSetting: updatedPullSetting
+        })
+      )
     } catch (e) {
-      yield put(actions.updatePullSettingFailed(serializeError(e)))
-      continue
+      yield put(
+        actions.updatePullSettingFailed(serializeError(e))
+      )
     }
   }
 }
-
 
 export default function*() {
   yield take('@@INITIALIZED')
