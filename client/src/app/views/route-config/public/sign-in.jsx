4fb72d11bdb28b39cba71a9fc8e5c90dd53e5f1c
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-10-11 15:38:45 +0700

    add sign in toast

--- client/src/app/views/route-config/public/sign-in.jsx
+++ client/src/app/views/route-config/public/sign-in.jsx
@@ -2,10 +2,11 @@ import { fork, put, race, take } from 'redux-saga/effects'
 
 import { actions, types } from 'state/interface'
 import SignIn from 'views/pages/sign-in'
+import { addToast } from 'state/saga/toast'
 
 const watchSignIn = function*(path) {
   while (true) {
-    yield take(types[ 'SESSION/CREATE' ])
+    yield take(types.session.CREATE)
 
     yield put(
       actions.mergeUIState(path, {
@@ -14,15 +15,19 @@ const watchSignIn = function*(path) {
     )
 
     const results = yield race({
-      completed: take(types[ 'SESSION/CREATE_COMPLETED' ]),
-      failed: take(types[ 'SESSION/CREATE_FAILED' ])
+      completed: take(types.session.CREATE_COMPLETED),
+      failed: take(types.session.CREATE_FAILED)
     })
 
+    if(results.failed) {
+      yield fork(addToast, {
+        type: 'error',
+        message: 'The information that you are provided does not match our record. Please check your email and password, then try again.'
+      })
+    }
     yield put(
       actions.replaceUIState(path, {
-        idle: true,
-        error: results.failed || null,
-        result: results.completed || null
+        idle: true
       })
     )
   }
