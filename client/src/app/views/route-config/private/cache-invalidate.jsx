586d5f6394bd8a4c24d17d0ba33c4f7daf1b65c7
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-10-15 14:58:06 +0700

    show cache invalid toast

--- client/src/app/views/route-config/private/cache-invalidate.jsx
+++ client/src/app/views/route-config/private/cache-invalidate.jsx
@@ -1,7 +1,24 @@
 import { all, fork, put, take, race, select } from 'redux-saga/effects'
 
+import { addToast } from 'state/saga/toast'
 import { actions, types, selectors } from 'state/interface'
 import * as CacheInvalidate from 'views/pages/cache-invalidate'
+
+const watchGetProject = function*() {
+  while (true) {
+    yield take(types.project.GET_FAILED)
+
+    yield all([
+      put(
+        actions.requestLocation('/projects')
+      ),
+      fork(addToast, {
+        type: 'error',
+        message: 'Project does not exist or internet connection error.'
+      })
+    ])
+  }
+}
 
 const watchCacheInvalidator = function*(path) {
   while (true) {
@@ -13,18 +30,30 @@ const watchCacheInvalidator = function*(path) {
       })
     )
 
-    const results = yield race({
+    const { completed, failed } = yield race({
       completed: take(types.project.INVALIDATE_CACHE_COMPLETED),
       failed: take(types.project.INVALIDATE_CACHE_FAILED)
     })
 
     yield put(
       actions.replaceUIState(path, {
-        idle: true,
-        error: results.failed || null,
-        result: results.completed || null
+        idle: true
       })
     )
+
+    if (completed) {
+      yield fork(addToast, {
+        type: 'success',
+        message: 'Cache invalidate completed. Please wait a minute to finish your change.'
+      })
+    }
+
+    if (failed) {
+      yield fork(addToast, {
+        type: 'error',
+        message: 'Cache invalidate failed. Please check your network connection and try again.'
+      })
+    }
   }
 }
 
@@ -33,6 +62,7 @@ export default {
     component: CacheInvalidate,
     exact: true,
     *state(path) {
+      yield fork(watchGetProject)
       yield fork(watchCacheInvalidator, path)
 
       const { identifier } = yield select(selectors.currentParams)
