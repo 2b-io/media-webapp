283df9c962026f6bff8edb50f06ae96ef561e2b0
Luc Hoang Long <i.me.mine@luchoanglong.com>
2018-10-19 21:38:30 +0700

    Update sendgrid.js

--- server/src/infrastructure/mailer/sendgrid.js
+++ server/src/infrastructure/mailer/sendgrid.js
@@ -2,5 +2,5 @@ import sgMail from '@sendgrid/mail'
 import config from 'infrastructure/config'
 
 sgMail.setApiKey(config.sendGrid)
-const sendgrid = (params) => { sgMail.send(params) }
-export default sendgrid
+
+export default sgMail
