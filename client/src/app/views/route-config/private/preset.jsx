d92ce59f77c8d89750b976caa161c3ceadf1204f
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-10-23 16:45:00 +0700

    prevent operation on create preset page

--- client/src/app/views/route-config/private/preset.jsx
+++ client/src/app/views/route-config/private/preset.jsx
@@ -4,18 +4,41 @@ import { addToast } from 'state/saga/toast'
 import { actions, selectors, types } from 'state/interface'
 import * as Preset from 'views/pages/preset'
 
-const watchGetProject = function*() {
-  yield take(types.project.GET_FAILED)
+const watchGetProject = function*(path) {
+  const { identifier } = yield select(selectors.currentParams)
 
-  yield all([
-    fork(addToast, {
-      type: 'error',
-      message: 'Cannot get your profile. Please check your network connection and try again.'
-    }),
-    put(
-      actions.requestLocation('/projects')
-    )
-  ])
+  const { completed, failed } = yield race({
+    completed: take(types.project.GET_COMPLETED),
+    failed: take(types.project.GET_FAILED)
+  })
+
+  if (failed) {
+    yield all([
+      fork(addToast, {
+        type: 'error',
+        message: 'Cannot connect to project. Project does not exist or network has error(s).'
+      }),
+      put(
+        actions.requestLocation('/projects')
+      )
+    ])
+  }
+
+  if (completed) {
+    const { isActive, status } = completed.payload.project
+
+    if (!(isActive === true && status === 'DEPLOYED')) {
+      yield all([
+        fork(addToast, {
+          type: 'error',
+          message: 'Project is initializing or disabled.'
+        }),
+        put(
+          actions.requestLocation(`/projects/${ identifier }`)
+        )
+      ])
+    }
+  }
 }
 
 const watchGetPreset = function*() {
@@ -163,7 +186,7 @@ export default {
     exact: true,
     *state(path) {
       yield fork(watchGetPreset, path)
-      yield fork(watchGetProject)
+      yield fork(watchGetProject, path)
       yield fork(watchRemovePreset, path)
       yield fork(watchUpdatePreset, path)
 
