d17fddd7d004f2106245e3fe9ef24ec9e41784bc
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-10-23 16:45:38 +0700

    prevent operation on edit pull setting page

--- client/src/app/views/route-config/private/pull-setting.jsx
+++ client/src/app/views/route-config/private/pull-setting.jsx
@@ -4,18 +4,41 @@ import { addToast } from 'state/saga/toast'
 import { actions, types, selectors } from 'state/interface'
 import * as PullSetting from 'views/pages/pull-setting'
 
-const watchGetProject = function*() {
-  yield take(types.project.GET_FAILED)
+const watchGetProject = function*(path) {
+  const { identifier } = yield select(selectors.currentParams)
 
-  yield all([
-    fork(addToast, {
-      type: 'error',
-      message: 'Project does not exist or internet connection has error.'
-    }),
-    put(
-      actions.requestLocation('/projects')
-    )
-  ])
+  const { completed, failed } = yield race({
+    completed: take(types.project.GET_COMPLETED),
+    failed: take(types.project.GET_FAILED)
+  })
+
+  if (failed) {
+    yield all([
+      fork(addToast, {
+        type: 'error',
+        message: 'Cannot connect to project. Project does not exist or network has error(s).'
+      }),
+      put(
+        actions.requestLocation('/projects')
+      )
+    ])
+  }
+
+  if (completed) {
+    const { isActive, status } = completed.payload.project
+
+    if (!(isActive === true && status === 'DEPLOYED')) {
+      yield all([
+        fork(addToast, {
+          type: 'error',
+          message: 'Project is initializing or disabled.'
+        }),
+        put(
+          actions.requestLocation(`/projects/${ identifier }`)
+        )
+      ])
+    }
+  }
 }
 
 const watchGetPullSetting = function*() {
