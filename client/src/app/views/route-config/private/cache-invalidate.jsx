a2dff572c4eaa100162a99367c738284c689d0e9
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-10-23 16:51:06 +0700

    prevent operation cache invalidate page

--- client/src/app/views/route-config/private/cache-invalidate.jsx
+++ client/src/app/views/route-config/private/cache-invalidate.jsx
@@ -4,18 +4,41 @@ import { addToast } from 'state/saga/toast'
 import { actions, types, selectors } from 'state/interface'
 import * as CacheInvalidate from 'views/pages/cache-invalidate'
 
-const watchGetProject = function*() {
-  yield take(types.project.GET_FAILED)
+const watchGetProject = function*(path) {
+  const { identifier } = yield select(selectors.currentParams)
 
-  yield all([
-    put(
-      actions.requestLocation('/projects')
-    ),
-    fork(addToast, {
-      type: 'error',
-      message: 'Project does not exist or internet connection has error.'
-    })
-  ])
+  const { completed, failed } = yield race({
+    completed: take(types.project.GET_COMPLETED),
+    failed: take(types.project.GET_FAILED)
+  })
+
+  if (failed) {
+    yield all([
+      fork(addToast, {
+        type: 'error',
+        message: 'Cannot connect to project. Project does not exist or network has error(s).'
+      }),
+      put(
+        actions.requestLocation('/projects')
+      )
+    ])
+  }
+
+  if (completed) {
+    const { isActive, status } = completed.payload.project
+
+    if (!(isActive === true && status === 'DEPLOYED')) {
+      yield all([
+        fork(addToast, {
+          type: 'error',
+          message: 'Project is initializing or disabled.'
+        }),
+        put(
+          actions.requestLocation(`/projects/${ identifier }`)
+        )
+      ])
+    }
+  }
 }
 
 const watchCacheInvalidator = function*(path) {
@@ -60,7 +83,7 @@ export default {
     component: CacheInvalidate,
     exact: true,
     *state(path) {
-      yield fork(watchGetProject)
+      yield fork(watchGetProject, path)
       yield fork(watchCacheInvalidator, path)
 
       const { identifier } = yield select(selectors.currentParams)
