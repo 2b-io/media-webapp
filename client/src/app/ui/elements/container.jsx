92e7a9c10ed1ff06fb8cea2ce9ac542bdc42ceb7
cuongtv <truongcuong90@gmail.com>
2018-11-05 19:45:28 +0700

    - update container

--- client/src/app/ui/elements/container.jsx
+++ client/src/app/ui/elements/container.jsx
@@ -1,7 +1,14 @@
+import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div`
+const MiddleContainer = styled.div`
+  max-width: 1024px;
+  margin: auto;
   padding: 16px;
 `
+
+const Container = ({ children }) => (
+  <MiddleContainer>{ children }</MiddleContainer>
+)
 
 export default Container
