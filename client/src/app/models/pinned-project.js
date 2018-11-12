11b734ad057df97817be76180a18f3fb663078fc
cuongtv <truongcuong90@gmail.com>
2018-11-12 16:06:21 +0700

    - Add metric to query

--- client/src/app/models/pinned-project.js
+++ client/src/app/models/pinned-project.js
@@ -1,22 +1,38 @@
 import request from 'services/graphql'
 import { PROJECT_FRAGMENT } from './project'
+import { USAGE_REPORT_FRAGMENT } from './metric'
 
 export default {
   async get(params, options) {
     const { token } = options
 
     const body = await request(`
-      query get($token: String!) {
+      query get($token: String!, $startTime: Float!, $endTime: Float! ,$period: Float!) {
         session(token: $token) {
           account {
             pinnedProjects {
               identifier
+              bytesDownloaded: metric(name: "BYTES_DOWNLOADED") {
+                name,
+                datapoints(startTime: $startTime, endTime: $endTime, period: $period) {
+                  ${ USAGE_REPORT_FRAGMENT }
+                }
+              },
+              requests: metric(name: "REQUESTS") {
+                name,
+                datapoints(startTime: $startTime, endTime: $endTime, period: $period) {
+                  ${ USAGE_REPORT_FRAGMENT }
+                }
+              }
             }
           }
         }
       }
     `, {
-      token
+      token,
+      startTime,
+      endTime,
+      period
     })
 
     return body.session.account.pinnedProjects
