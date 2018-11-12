575e2b507180dc096629c6907606284bd88ef17e
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-11-12 18:39:05 +0700

    use responsive grid on usage report form

--- client/src/app/views/pages/usage-report/form.jsx
+++ client/src/app/views/pages/usage-report/form.jsx
@@ -2,9 +2,15 @@ import ms from 'ms'
 import React from 'react'
 
 import dateTimeService from 'services/date-time'
-import { Break, Button, Form } from 'ui/elements'
+import { Break, Button, Form, ResponsiveGrid } from 'ui/elements'
 import { DatePicker, Select } from 'views/common/form'
 import { validateRequired } from 'views/common/validate'
+
+const BREAK_POINTS = {
+  phone: 1,
+  tablet: 2,
+  desktop: 4
+}
 
 const UsageReportForm = ({
   currentDateRange,
@@ -17,38 +23,58 @@ const UsageReportForm = ({
 
   return (
     <Form handleSubmit={ handleSubmit } idle={ idle }>
-      <DatePicker
-        disabled={ !idle }
-        name="startDate"
-        label="Start Date"
-        type="date"
-        validate={ validateRequired }
-        min={ min }
-        max={ currentDateRange.endDate }
-      />
-      <Break />
-      <DatePicker
-        disabled={ !idle }
-        name="endDate"
-        label="End date"
-        type="date"
-        validate={ validateRequired }
-        min={ currentDateRange.startDate }
-        max={ max }
-      />
-      <Break />
-      <Select
-        disabled={ !idle }
-        name="granularity"
-        label="Granularity (Daily or Hourly)."
-        options={ options.granularity }
-      />
-      <Break />
-      <Select
-        disabled={ !idle }
-        name="projectIdentifier"
-        label="Project"
-        options={ options.projects }
+      <ResponsiveGrid
+        breakpoints={ BREAK_POINTS }
+        items={
+          [
+            {
+              content: () => (
+                <DatePicker
+                  disabled={ !idle }
+                  name="startDate"
+                  label="Start Date"
+                  type="date"
+                  validate={ validateRequired }
+                  min={ min }
+                  max={ currentDateRange.endDate }
+                />
+              )
+            },
+            {
+              content: () => (
+                <DatePicker
+                  disabled={ !idle }
+                  name="endDate"
+                  label="End date"
+                  type="date"
+                  validate={ validateRequired }
+                  min={ currentDateRange.startDate }
+                  max={ max }
+                />
+              )
+            },
+            {
+              content: () => (
+                <Select
+                  disabled={ !idle }
+                  name="granularity"
+                  label="Granularity (Daily or Hourly)."
+                  options={ options.granularity }
+                />
+              )
+            },
+            {
+              content: () => (
+                <Select
+                  disabled={ !idle }
+                  name="projectIdentifier"
+                  label="Project"
+                  options={ options.projects }
+                />
+              )
+            }
+          ]
+        }
       />
       <Break double />
       <Button
