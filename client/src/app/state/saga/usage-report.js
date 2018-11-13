7c5ca213c2b8bd3057e51db90b9ca8a3fbee1090
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-11-13 08:48:03 +0700

    move synthesize data to Service

--- client/src/app/state/saga/usage-report.js
+++ client/src/app/state/saga/usage-report.js
@@ -3,43 +3,10 @@ import { fork, put, select, take } from 'redux-saga/effects'
 import serializeError from 'serialize-error'
 
 import dateTimeService from 'services/date-time'
+import syntheticDataService from 'services/synthetic-data'
 import { actions, types, selectors } from 'state/interface'
 
 import Metric from 'models/metric'
-
-const synthesizeRequestData = (datapoints) => {
-  if (!datapoints.length) {
-    return {
-      average: 0,
-      total: 0,
-      maximum: 0,
-      minimum: 0
-    }
-  }
-
-  const values = datapoints.map((item) => item.value)
-  const minimum = Math.min(...values)
-  const maximum = Math.max(...values)
-  const total = values.reduce((a, b) => a + b)
-  const average = Math.trunc(total / values.length)
-
-  return {
-    average,
-    total,
-    maximum,
-    minimum
-  }
-}
-
-const synthesizeBytesDownloadData = (datapoints) => {
-  if (!datapoints.length) {
-    return { totalBytes: 0 }
-  }
-
-  const totalBytes = datapoints.map((item) => item.value).reduce((total, value) => total + value)
-
-  return { totalBytes }
-}
 
 const generateUsageReportLoop = function*() {
   while (true) {
@@ -78,8 +45,8 @@ const generateUsageReportLoop = function*() {
         requests
       } = data
 
-      const bytesDownloadData = synthesizeBytesDownloadData(bytesDownloaded.datapoints)
-      const requestData = synthesizeRequestData(requests.datapoints)
+      const bytesDownloadData = syntheticDataService.synthesizeBytesDownloadData(bytesDownloaded.datapoints)
+      const requestData = syntheticDataService.synthesizeRequestData(requests.datapoints)
 
       yield put(
         actions.generateUsageReportCompleted(data, granularity, requestData, bytesDownloadData)
