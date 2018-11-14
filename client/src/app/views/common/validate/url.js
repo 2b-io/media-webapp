c68bd14a2796a5dfd6cc1c2074ee5f0e3cc5b949
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-11-14 08:46:06 +0700

    add validate url

---
+++ client/src/app/views/common/validate/url.js
@@ -0,0 +1,10 @@
+const urlRegex = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$', 'i') // fragment locater
+
+const validURL = (url) => url && !urlRegex.test(url) ? 'Invalid url' : undefined
+
+export default validURL
