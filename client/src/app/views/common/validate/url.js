faae053e4c18215f13bf0637f8272c23082e6ff5
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-11-14 15:04:27 +0700

    fix eslint

--- client/src/app/views/common/validate/url.js
+++ client/src/app/views/common/validate/url.js
@@ -1,4 +1,4 @@
-//use url regex on this site: https://runkit.com/shanekenyon87/5bc0e57263c77b0012db05dc
+/* use url regex on this site: https://runkit.com/shanekenyon87/5bc0e57263c77b0012db05dc
 const urlRegex = new RegExp('^(https?:\\/\\/)?'+ // protocol
   '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
   '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
@@ -6,6 +6,8 @@ const urlRegex = new RegExp('^(https?:\\/\\/)?'+ // protocol
   '(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
   '(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
   '(\\#[-a-z\\d_]*)?$', 'i') // fragment locator
+*/
+const urlRegex = /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%@_.~+&:]*)*(\?[;&a-z\d%@_.,~+&:=-]*)?(\#[-a-z\d_]*)?$/i
 
 const validUrl = (url) => url && !urlRegex.test(url) ? 'Invalid url' : undefined
 
