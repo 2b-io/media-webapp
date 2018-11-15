abc4a76b8ce1b41cbfea33a5f3f22889cb0aa5c7
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-11-15 11:43:41 +0700

    added plain button

---
+++ client/src/app/ui/elements/button/plain-button.jsx
@@ -0,0 +1,38 @@
+import styled, { css } from 'styled-components'
+
+const PlainButton = styled.button.attrs({
+  type: ({ type = 'button' }) => type
+})`
+  appearance: none;
+  border: none;
+  outline: none;
+  background: transparent;
+  margin: 0;
+  white-space: nowrap;
+  line-height: 40px;
+  height: 40px;
+  cursor: ${
+    ({
+      disabled,
+      theme: { mouseDetected }
+    }) => disabled ? 'not-allowed' : (
+      mouseDetected ? 'pointer' : 'unset'
+    )
+  };
+
+  ${
+    ({ theme }) => theme.mouseDetected && !theme.touchDetected && css`
+      transition:  opacity .3s;
+
+      &:hover {
+        opacity: 0.7;
+      }
+    `
+  };
+
+  &:focus {
+    outline: none;
+  }
+`
+
+export default PlainButton
