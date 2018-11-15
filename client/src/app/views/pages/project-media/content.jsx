0da2896352af767097d8d0eb8bdb6867a89c7fac
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-11-15 13:57:09 +0700

    fixed eslint

--- client/src/app/views/pages/project-media/content.jsx
+++ client/src/app/views/pages/project-media/content.jsx
@@ -8,7 +8,7 @@ import { mapDispatch } from 'services/redux-helpers'
 import { mapState } from 'services/redux-helpers'
 import { actions, selectors } from 'state/interface'
 import { Panel, TitleBar } from 'ui/compounds'
-import { Button, Container, MasonryLayout, Paragraph } from 'ui/elements'
+import { Container, MasonryLayout, Paragraph, PlainButton } from 'ui/elements'
 import { CopyIcon } from 'ui/icons'
 
 const MediaImage = styled.div`
