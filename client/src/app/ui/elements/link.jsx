a9c9415d1cc948850c861583caa6029a574c7143
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-11-16 14:17:31 +0700

    update style for Link

--- client/src/app/ui/elements/link.jsx
+++ client/src/app/ui/elements/link.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import preventDefault from 'services/prevent-default'
 
 const StyledLink = styled.a`
   text-decoration: underline;
-  cursor: ${
-    ({
-      disabled,
-      theme: { mouseDetected }
-    }) => disabled ? 'not-allowed' : (
-      mouseDetected ? 'pointer' : 'unset'
-    )
-  };
-  &:hover {
-    opacity: ${
-      ({ theme }) => theme.mouseDetected && !theme.touchDetected ?
-        '0.7' :
-        'unset'
-    }
+
+  ${
+    ({ disabled, theme: { mouseDetected } }) => mouseDetected && css`
+      cursor: ${ disabled ? 'not-allowed' : 'pointer' };
+    `
   }
+  ${
+    ({ disabled, theme }) => css`
+      ${
+        !disabled && theme.mouseDetected && !theme.touchDetected && css`
+          transition: opacity .3s;
+
+          &:hover {
+            opacity: 0.7;
+          }
+        `
+      }
+    `
+  }
+
 `
 
 const Link = ({ onClick, ...props }) => (
