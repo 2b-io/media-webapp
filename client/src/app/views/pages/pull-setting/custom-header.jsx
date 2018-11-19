c7efe4b7a17c285af6cff762c351ff770e518c7f
LongLH <d@dapps.me>
2018-11-19 10:23:13 +0700

    use ResponsiveGrid in PullSetting screen

--- client/src/app/views/pages/pull-setting/custom-header.jsx
+++ client/src/app/views/pages/pull-setting/custom-header.jsx
@@ -1,26 +1,9 @@
 import React from 'react'
 import { FieldArray } from 'redux-form'
-import styled from 'styled-components'
 
 import { AddIcon, TrashIcon } from 'ui/icons'
-import { PlainButton } from 'ui/elements'
+import { PlainButton, ResponsiveGrid } from 'ui/elements'
 import { TextBox } from 'views/common/form'
-
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 40px;
-  grid-column-gap: 8px
-  & > * {
-    min-width: 0;
-    min-height: 0;
-  }
-  position: relative;
-`
-const WrapperButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
 
 const CustomHeaderList = ({
   fields,
@@ -28,36 +11,46 @@ const CustomHeaderList = ({
 }) => (
   fields.map(
     (header, index) => (
-      <Wrapper key={ index }>
-        <TextBox
-          disabled={ !idle }
-          label="Header Name"
-          type="text"
-          name={ `${ header }.name` }
-        />
-        <TextBox
-          disabled={ !idle }
-          label="Header Value"
-          type="text"
-          name={ `${ header }.value` }
-        />
-        <WrapperButton>
-          { index === fields.length - 1 ?
-            <PlainButton
+      <ResponsiveGrid key={ index }
+        breakpoints={ { otherwise: '1fr 1fr 40px' } }
+        items={ [ {
+          content: () => (
+            <TextBox
               disabled={ !idle }
-              onClick={ () => fields.push({}) }
-            >
-              <AddIcon />
-            </PlainButton> :
-            <PlainButton
+              label="Header Name"
+              type="text"
+              name={ `${ header }.name` }
+            />
+          )
+        }, {
+          content: () => (
+            <TextBox
               disabled={ !idle }
-              onClick={ () => fields.remove(index) }
-            >
-              <TrashIcon />
-            </PlainButton>
-          }
-        </WrapperButton>
-      </Wrapper>
+              label="Header Value"
+              type="text"
+              name={ `${ header }.value` }
+            />
+          )
+        }, {
+          content: index === fields.length - 1 ?
+            () => (
+              <PlainButton
+                disabled={ !idle }
+                onClick={ () => fields.push({}) }
+              >
+                <AddIcon />
+              </PlainButton>
+            ) :
+            () => (
+              <PlainButton
+                disabled={ !idle }
+                onClick={ () => fields.remove(index) }
+              >
+                <TrashIcon />
+              </PlainButton>
+            )
+        } ] }
+      />
     )
   )
 )
