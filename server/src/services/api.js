8e4eff4f590dd22081a2f36966a75c640563458b
Luc Hoang Long <i.me.mine@luchoanglong.com>
2018-11-28 15:39:19 +0700

    review code

--- server/src/services/api.js
+++ server/src/services/api.js
@@ -2,12 +2,6 @@ import request from 'superagent'
 
 import config from 'infrastructure/config'
 
-const joinParam = (params) => {
-  return Object.entries(params)
-    .map((entry) => entry[ 1 ] && entry.join('='))
-    .filter(Boolean).join(',')
-}
-
 class ApiService {
   constructor(appIdentifier, accountIdentifier) {
     this.appIdentifier = appIdentifier
@@ -15,13 +9,15 @@ class ApiService {
   }
 
   async callApi(method, path, body) {
-    const apiPath = config.apiUrl + path
-    const authParams = joinParam({
+    const authParams = Object.entries({
       app: this.appIdentifier,
       account: this.accountIdentifier
     })
+    .map((entry) => entry[ 1 ] && entry.join('='))
+    .filter(Boolean)
+    .join(',')
 
-    const response = await request(method, apiPath)
+    const response = await request(method, `${ config.apiUrl }${ path }`)
       .set('Content-Type', 'application/json')
       .set('Authorization', `MEDIA_CDN ${ authParams }`)
       .send(body)
