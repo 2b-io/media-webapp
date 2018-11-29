9aef6d1f17b1aa2376eb6dbc8382c71e2963c43c
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-11-29 17:17:58 +0700

    use update cache setting api

--- server/src/services/cache-setting.js
+++ server/src/services/cache-setting.js
@@ -39,6 +39,10 @@ class CacheSettingService extends ApiService {
   async get(projectIdentifier) {
     return await this.callApi('get', `/projects/${ projectIdentifier }/cache-setting`)
   }
+
+  async update(projectIdentifier, body) {
+    return await this.callApi('put', `/projects/${ projectIdentifier }/cache-setting`, body)
+  }
 }
 
 export default (accountIdentifier) => {
