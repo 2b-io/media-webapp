852ecb1ed2f5af13b2e89f38756f64b0b2e357a4
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-11-30 14:38:55 +0700

    update infrastructure job service

--- server/src/services/infrastructure-job.js
+++ server/src/services/infrastructure-job.js
@@ -1,16 +1,20 @@
-import request from 'superagent'
-
 import config from 'infrastructure/config'
+import ApiServices from 'services/api'
 
-export default async (projectIdentifier) => {
-  return await request
-    .post(`${ config.jobServer }/jobs`)
-    .set('Content-Type', 'application/json')
-    .send({
+class InfrastructureJobService extends ApiServices {
+  async create(projectIdentifier) {
+    const body = {
       name: 'CHECK_INFRASTRUCTURE',
       when: Date.now(),
       payload: {
         projectIdentifier
       }
-    })
+    }
+
+    return await this.callApi('post', `${ config.apiUrl }/jobs`, body)
+  }
+}
+
+export default (accountIdentifier) => {
+  return new InfrastructureJobService('webapp', accountIdentifier)
 }
