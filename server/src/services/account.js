b1abe73e62182553c5bce20e07956531f30048e7
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-12-18 18:08:36 +0700

    rebase session

--- server/src/services/account.js
+++ server/src/services/account.js
@@ -1,4 +1,21 @@
+import escapeStringRegexp from 'escape-string-regexp'
+
+import Account from 'models/account'
 import ApiService from 'services/api'
+
+export const searchByEmail = async (email) => {
+  if (!email) {
+    throw new Error('Invaid parameter')
+  }
+  const escapeString = escapeStringRegexp(email)
+
+  //regex to describes a pattern of character: matches beginning of email
+  const emailRegex = new RegExp(`^${ escapeString }`)
+
+  return await Account.find({ email: emailRegex })
+}
+
+//use new api
 
 class AccountService extends ApiService {
   async changePassword(identifier, body) {
