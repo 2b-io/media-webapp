045e8eb2f75d218d5aac54351b4afa4a5ab763a3
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-12-25 18:38:57 +0700

    remove project detail old code

--- client/src/app/views/route-config/private/project-detail.jsx
+++ client/src/app/views/route-config/private/project-detail.jsx
@@ -4,73 +4,6 @@ import { addToast } from 'state/saga/toast'
 import { actions, selectors, types } from 'state/interface'
 import * as ProjectDetail from 'views/pages/project-detail'
 
-const watchGetProject = function*(path) {
-  const { completed, failed } = yield race({
-    completed: take(types.project.GET_COMPLETED),
-    failed: take(types.project.GET_FAILED)
-  })
-
-  if (failed) {
-    yield all([
-      fork(addToast, {
-        type: 'error',
-        message: 'Cannot connect to project. Project does not exist or network has error(s).'
-      }),
-      put(
-        actions.requestLocation('/projects')
-      )
-    ])
-  }
-
-  if (completed) {
-    const { isActive, status } = completed.payload.project
-
-    if (isActive === true && status === 'DEPLOYED') {
-      yield put(
-        actions.mergeUIState(path, {
-          isProjectActive: true
-        })
-      )
-    }
-  }
-}
-
-const watchGetCacheSetting = function*() {
-  yield take(types.cacheSetting.GET_FAILED)
-
-  yield fork(addToast, {
-    type: 'error',
-    message: 'Get cache setting failed.'
-  })
-}
-
-const watchFetchPreset = function*() {
-  yield take(types.preset.FETCH_FAILED)
-
-  yield fork(addToast, {
-    type: 'error',
-    message: 'Fetch presets failed.'
-  })
-}
-
-const watchFetchSecretKey = function*() {
-  yield take(types.secretKey.FETCH_FAILED)
-
-  yield fork(addToast, {
-    type: 'error',
-    message: 'Fetch secret key failed.'
-  })
-}
-
-const watchGetPullSetting = function*() {
-  yield take(types.pullSetting.GET_FAILED)
-
-  yield fork(addToast, {
-    type: 'error',
-    message: 'Get pull setting failed.'
-  })
-}
-
 const watchCreatePreset = function*(path) {
   while (true) {
     yield take(`${ types.dialog.SHOW }:CREATE_PRESET`)
@@ -224,83 +157,83 @@ const watchMakeOwner = function*(path) {
   }
 }
 
-const watchCreateSecretKey = function*() {
-  while (true) {
-    const { createCompleted, createFailed } = yield race({
-      createCompleted: take(types.secretKey.CREATE_COMPLETED),
-      createFailed: take(types.secretKey.CREATE_FAILED)
-    })
+// const watchCreateSecretKey = function*() {
+//   while (true) {
+//     const { createCompleted, createFailed } = yield race({
+//       createCompleted: take(types.secretKey.CREATE_COMPLETED),
+//       createFailed: take(types.secretKey.CREATE_FAILED)
+//     })
 
-    if (createCompleted) {
-      yield fork(addToast, {
-        expiring: '5s',
-        type: 'success',
-        message: 'A new API key created.'
-      })
-    }
+//     if (createCompleted) {
+//       yield fork(addToast, {
+//         expiring: '5s',
+//         type: 'success',
+//         message: 'A new API key created.'
+//       })
+//     }
 
-    if (createFailed) {
-      yield fork(addToast, {
-        type: 'error',
-        message: 'Cannot create API key. Please check your internet connection and try again.'
-      })
-    }
-  }
-}
+//     if (createFailed) {
+//       yield fork(addToast, {
+//         type: 'error',
+//         message: 'Cannot create API key. Please check your internet connection and try again.'
+//       })
+//     }
+//   }
+// }
 
-const watchUpdateSecretKey = function*() {
-  while (true) {
-    const {
-      payload: {
-        secretKey: { isActive }
-      }
-    } = yield take(types.secretKey.UPDATE)
+// const watchUpdateSecretKey = function*() {
+//   while (true) {
+//     const {
+//       payload: {
+//         secretKey: { isActive }
+//       }
+//     } = yield take(types.secretKey.UPDATE)
 
-    const { updateCompleted, updateFailed } = yield race({
-      updateCompleted: take(types.secretKey.UPDATE_COMPLETED),
-      updateFailed: take(types.secretKey.UPDATE_FAILED)
-    })
+//     const { updateCompleted, updateFailed } = yield race({
+//       updateCompleted: take(types.secretKey.UPDATE_COMPLETED),
+//       updateFailed: take(types.secretKey.UPDATE_FAILED)
+//     })
 
-    if (updateCompleted) {
-      yield fork(addToast, {
-        expiring: '5s',
-        type: 'success',
-        message: `Sucessfully ${ isActive ? 'enable' : 'disable' } the API key.`
-      })
-    }
+//     if (updateCompleted) {
+//       yield fork(addToast, {
+//         expiring: '5s',
+//         type: 'success',
+//         message: `Sucessfully ${ isActive ? 'enable' : 'disable' } the API key.`
+//       })
+//     }
 
-    if (updateFailed) {
-      yield fork(addToast, {
-        type: 'error',
-        message: `Can not ${ isActive ? 'enable' : 'disable' } the API key. Please check your internet connection and try again.`
-      })
-    }
-  }
-}
+//     if (updateFailed) {
+//       yield fork(addToast, {
+//         type: 'error',
+//         message: `Can not ${ isActive ? 'enable' : 'disable' } the API key. Please check your internet connection and try again.`
+//       })
+//     }
+//   }
+// }
 
-const watchRemoveSecretKey = function*() {
-  while (true) {
-    const { removeCompleted, removeFailed } = yield race({
-      removeCompleted: take(types.secretKey.REMOVE_COMPLETED),
-      removeFailed: take(types.secretKey.REMOVE_FAILED)
-    })
+// const watchRemoveSecretKey = function*() {
+//   while (true) {
+//     const { removeCompleted, removeFailed } = yield race({
+//       removeCompleted: take(types.secretKey.REMOVE_COMPLETED),
+//       removeFailed: take(types.secretKey.REMOVE_FAILED)
+//     })
 
-    if (removeCompleted) {
-      yield fork(addToast, {
-        expiring: '5s',
-        type: 'success',
-        message: 'Sucessfully remove the API key.'
-      })
-    }
+//     if (removeCompleted) {
+//       yield fork(addToast, {
+//         expiring: '5s',
+//         type: 'success',
+//         message: 'Sucessfully remove the API key.'
+//       })
+//     }
 
-    if (removeFailed) {
-      yield fork(addToast, {
-        type: 'error',
-        message: 'Can not remove the API key. Please check your internet connection and try again.'
-      })
-    }
-  }
-}
+//     if (removeFailed) {
+//       yield fork(addToast, {
+//         type: 'error',
+//         message: 'Can not remove the API key. Please check your internet connection and try again.'
+//       })
+//     }
+//   }
+// }
 
 const watchGetProjectDetail = function*(path) {
   const { completed, failed } = yield race({
@@ -321,7 +254,6 @@ const watchGetProjectDetail = function*(path) {
   }
 
   if (completed) {
-    console.log('completed.payload.project', completed.payload.project);
     const {
       isActive,
       status,
@@ -350,14 +282,6 @@ export default {
     exact: true,
     *state(path) {
       yield fork(watchGetProjectDetail, path)
-      // yield fork(watchGetProject, path)
-      // yield fork(watchFetchPreset)
-      // // yield fork(watchFetchSecretKey)
-      // yield fork(watchGetPullSetting)
-      // yield fork(watchGetCacheSetting)
-      //yield fork(watchCreateSecretKey)
-      // yield fork(watchRemoveSecretKey)
-      // yield fork(watchUpdateSecretKey)
       yield fork(watchCreatePreset, path)
       yield fork(watchLeaveProject, path)
       yield fork(watchMakeOwner, path)
@@ -369,18 +293,6 @@ export default {
         put(
           actions.getProject(identifier)
         ),
-        // put(
-        //   actions.fetchPresets({ identifier })
-        // ),
-        // put(
-        //   actions.getCacheSetting(identifier)
-        // ),
-        // put(
-        //   actions.getPullSetting(identifier)
-        // ),
-        // put(
-        //   actions.fetchSecretKeys(identifier)
-        // ),
         put(
           actions.initializeUIState(path, {
             idle: true,
