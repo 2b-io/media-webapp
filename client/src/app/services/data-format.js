5c5e0135af76f2c2425659800270790ae09bc0f0
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2019-01-07 16:04:14 +0700

    add function format milisecond to string

--- client/src/app/services/data-format.js
+++ client/src/app/services/data-format.js
@@ -1,5 +1,6 @@
 import dateFormat from 'dateformat'
 import humanFormat from 'human-format'
+import ms from 'ms'
 import prettyBytes from 'pretty-bytes'
 
 const formatTime = (time, format) => dateFormat(time, format)
@@ -8,8 +9,24 @@ const formatNumber = (value) => humanFormat(value)
 
 const formatSize = (value) => prettyBytes(value)
 
+const formatMilisecondToString = (time, format) => {
+  if (format) {
+    const hour = Math.floor(time / ms('1h'))
+    const min = Math.floor((time % ms('1h')) / ms('1m'))
+    const sec = Math.floor((time % ms('1m')) / ms('1s'))
+
+    return `${ hour > 9 ? hour : '0'+hour }h:
+      ${ min > 9 ? min : '0'+min }m:
+      ${ sec > 9 ? sec : '0'+sec }s
+    `
+  }
+
+  return `${ Math.floor(time / ms('1000')) }s`
+}
+
 export default {
   formatNumber,
   formatTime,
-  formatSize
+  formatSize,
+  formatMilisecondToString
 }
