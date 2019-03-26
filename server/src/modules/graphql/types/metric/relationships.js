54d6b537773eb4fab37de92996273b51c7329542
cuongtv <truongcuong90@gmail.com>
2019-03-26 16:22:26 +0700

    Paging when get data from elasticsearch

--- server/src/modules/graphql/types/metric/relationships.js
+++ server/src/modules/graphql/types/metric/relationships.js
@@ -4,10 +4,61 @@ import {
 } from 'graphql'
 
 import createCloudwatchService from 'services/cloudwatch'
+import searchDataAll from 'services/search-data-all'
 import { Datapoint } from '../datapoint'
 
 const compareDateTimeString = (firstDateTime, secondDateTime) => {
   return new Date(firstDateTime) - new Date(secondDateTime)
+}
+
+const generateRangeTimes = (startTime, endTime) => {
+  let timePoint = startTime
+  let dates = []
+
+  while (timePoint < endTime && timePoint < Date.now()) {
+    dates.push(timePoint)
+    timePoint += 60 * 1000
+  }
+
+  return dates
+}
+
+const formatDataMetric = (metricData, { startTime, endTime, period }) => {
+  const originRangeTimes = generateRangeTimes(Number(startTime), Number(endTime), Number(period))
+
+  const indices = metricData.reduce(
+    (indices, datapoint ) => ({
+      ...indices,
+      [ new Date(datapoint.timestamp).getTime() ]: datapoint.value
+    }),
+    {}
+  )
+
+  const originDatapoints = originRangeTimes.map((time) => ({
+    timestamp: time,
+    value: indices[ time ] || 0
+  }))
+
+  let datapoints = []
+
+  originDatapoints.forEach(
+    (datapoint) => {
+      const { timestamp, value } = datapoint
+
+      const index = Math.floor((timestamp - startTime) / (period * 60 * 1000))
+
+      if (!datapoints[ index ]) {
+        datapoints[ index ] = {
+          timestamp: new Date(timestamp).toISOString(),
+          value: 0
+        }
+      }
+
+      datapoints[ index ].value += value
+    }
+  )
+
+  return datapoints
 }
 
 export default () => ({
@@ -26,12 +77,23 @@ export default () => ({
     type: new GraphQLList(Datapoint),
     resolve: async ({ projectIdentifier, name }, { startTime, endTime, period }, ctx) => {
       const cloudwatchService = createCloudwatchService(ctx._session.account.identifier)
-      const datapoints = await cloudwatchService.metricCloudfront(
-        projectIdentifier,
-        name.toLowerCase(),
-        startTime,
-        endTime,
-        period
+      const metricData = await searchDataAll(
+        cloudwatchService.metricCloudfront(
+            projectIdentifier,
+            name.toLowerCase(),
+            startTime,
+            endTime,
+            period
+        )
+      )
+
+      const datapoints = await formatDataMetric(
+        metricData,
+        {
+          startTime,
+          endTime,
+          period
+        }
       )
 
       return datapoints.map(({
